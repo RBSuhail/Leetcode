8d4ed616baa2fab97d877ec02029374410c0d9d8
RB SUHAIL <114150506+RBSuhail@users.noreply.github.com>
2024-08-03 10:12:03 +0530

    Time: 2261 ms (5.01%), Space: 14.1 MB (13.9%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.cpp
+++ 0010-regular-expression-matching/0010-regular-expression-matching.cpp
@@ -1,6 +1,11 @@
 class Solution {
 public:
-    bool isMatch(string s, string p) {
-        
+    bool solve(const string& s, const string& p) {
+        if (p.empty()) return s.empty();
+        bool isMatchFirstChar = !s.empty() && (s[0] == p[0] || p[0] == '.');
+        if (p.size() > 1 && p[1] == '*') {
+        return solve(s, p.substr(2))||(isMatchFirstChar && solve(s.substr(1), p));
+        }return isMatchFirstChar && solve(s.substr(1), p.substr(1));
     }
+    bool isMatch(const string& s, const string& p) { return solve(s, p); }
 };
