754c27bb7e78c280f832a68ee59297691ea6bd63
RB SUHAIL <114150506+RBSuhail@users.noreply.github.com>
2024-08-04 08:57:29 +0530

    Time: 298 ms (36.36%), Space: 181.6 MB (45.45%) - LeetHub

--- 3526-minimum-number-of-flips-to-make-binary-grid-palindromic-i/3526-minimum-number-of-flips-to-make-binary-grid-palindromic-i.cpp
+++ 3526-minimum-number-of-flips-to-make-binary-grid-palindromic-i/3526-minimum-number-of-flips-to-make-binary-grid-palindromic-i.cpp
@@ -1,54 +1,35 @@
 class Solution {
 public:
-    int checkForRow(vector<vector<int>>&grid,int n , int m){
-        int count=0;
-        for(int i=0;i<n;i++){
-            int start = 0;
-            int end = m-1;
-            
-            while(start <= end){
-                if(grid[i][start] != grid[i][end]){
-                    count++;
-                    start++;
-                    end--;
-                }else start++,end--;
-            }
-        }
-        
-        return count;
-    }
-    int checkForCol(vector<vector<int>>&grid,int n , int m){
-        
-        int count = 0;
-        for(int i=0;i<m;i++){
-            int start =0;
-            int end = n-1 ;
-            
-            while(start<=end){
-                if(grid[start][i] != grid[end][i]){
-                    count++;
-                    start++;
-                    end--;
-                }else{
-                    start++;
-                    end--;
+    int minFlips(vector<vector<int>>& grid) {
+        int r = grid.size();
+        int c = grid[0].size();
+
+        int rowFlips = 0;
+        int colFlips = 0;
+
+        // Calculate flips needed to make all rows palindromic
+        for (int i = 0; i < r; i++) {
+            int flips = 0;
+            for (int j = 0; j < c / 2; j++) {
+                if (grid[i][j] != grid[i][c - 1 - j]) {
+                    flips++;
                 }
             }
+            rowFlips += flips;
         }
+
+        // Calculate flips needed to make all columns palindromic
+        for (int j = 0; j < c; j++) {
+            int flips = 0;
+            for (int i = 0; i < r / 2; i++) {
+                if (grid[i][j] != grid[r - 1 - i][j]) {
+                    flips++;
+                }
+            }
+            colFlips += flips;
+        }
+
         
-        return count;
-    }
-    int minFlips(vector<vector<int>>& grid) {
-        
-        int n = grid.size();
-        int m = grid[0].size();
-        
-        int countCol = checkForCol(grid,n,m);
-        int countRow = checkForRow(grid,n,m);
-        
-        return min(countCol, countRow);
-        
-        
-        
+        return min(rowFlips, colFlips);
     }
 };
