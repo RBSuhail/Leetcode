6a00c462000373f26a8b78008fa487f15d833862
RB SUHAIL <114150506+RBSuhail@users.noreply.github.com>
2024-08-09 18:05:32 +0530

    Time: 23 ms (50.87%), Space: 11 MB (34.17%) - LeetHub

---
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    string minWindow(string s, string t) {
+        unordered_map<char, int> mp;
+        for (char c : t) mp[c]++;
+        int required = t.size();  // Total number of characters to match
+        int minLength = INT_MAX, startIdx = 0;
+        int i = 0, j = 0;
+
+        for (; j < s.size(); j++) {
+            if (mp.find(s[j]) != mp.end()) {
+                if (mp[s[j]] > 0) {
+                    required--;  // Decrease the required count only when a needed character is found
+                }
+                mp[s[j]]--;  // Decrease the frequency in the map
+            }
+
+           for(;required == 0;i++) {  // All characters are matched
+                if (j - i + 1 < minLength) {
+                    minLength = j - i + 1;
+                    startIdx = i;
+                }
+
+                if (mp.find(s[i]) != mp.end()) {
+                    mp[s[i]]++;  // Increase the frequency as the character is about to be removed from the window
+                    if (mp[s[i]] > 0) {
+                        required++;  // Increase the required count if a needed character is removed
+                    }
+                }
+
+                  // Shrink the window from the left
+            }
+        }
+
+        return (minLength == INT_MAX) ? "" : s.substr(startIdx, minLength);
+    }
+};
