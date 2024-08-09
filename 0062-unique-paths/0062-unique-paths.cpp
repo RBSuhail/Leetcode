0cef9ebcad7ae6fb494f6658a09f712941911fa0
RB SUHAIL <114150506+RBSuhail@users.noreply.github.com>
2024-08-09 12:07:01 +0530

    Time: 0 ms (100%), Space: 7.6 MB (59.63%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,28 +1,14 @@
-#include <vector>
-#include <cstring>
-
 class Solution {
 public:
-    
-
+int t[200][200];
     int uniquePaths(int m, int n) {
-       int t[200][200]={-1};
-
-        // Initialize the first row and first column
+        memset(t,-1,sizeof(t));
         for (int i = 0; i < m; ++i) {
-            t[i][0] = 1;
-        }
-        for (int j = 0; j < n; ++j) {
-            t[0][j] = 1;
-        }
-
-        // Fill the rest of the table
-        for (int i = 1; i < m; ++i) {
-            for (int j = 1; j < n; ++j) {
-                t[i][j] = t[i - 1][j] + t[i][j - 1];
+            for (int j = 0; j < n; ++j) {
+                if(i==0||j==0)t[i][j]=1;
+                else t[i][j] = t[i - 1][j] + t[i][j - 1];
             }
         }
-
         return t[m - 1][n - 1];
     }
 };
