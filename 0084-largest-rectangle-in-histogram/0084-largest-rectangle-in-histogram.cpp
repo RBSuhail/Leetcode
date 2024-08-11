8e3a63d9b113016853321bea137c7a98bdbdafe1
RB SUHAIL <114150506+RBSuhail@users.noreply.github.com>
2024-08-11 14:31:19 +0530

    Time: 121 ms (43%), Space: 96.3 MB (5.68%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -2,11 +2,11 @@ class Solution {
 private:
     vector<int> nextSmallerElement(vector<int> arr, int n) {
         stack<int> s;
-        s.push(-1);
+        s.push(n);
         vector<int> ans(n);
        for(int i=n-1; i>=0 ; i--) {
             int curr = arr[i];
-            while(s.top() != -1 && arr[s.top()] >= curr)
+            while(s.top() != n && arr[s.top()] >= curr)
             s.pop();
             ans[i] = s.top();
             s.push(i);
@@ -35,9 +35,6 @@ public:
         int area = INT_MIN;
         for(int i=0; i<n; i++) {
             int l = heights[i];
-            if(next[i] == -1) {
-                next[i] = n;
-            }
              int b = next[i] - prev[i] - 1;
             int newArea = l*b;
             area = max(area, newArea);
