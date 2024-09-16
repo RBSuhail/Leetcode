55610088499a76f67de1346faba815e7daf38ddf
RB SUHAIL <114150506+RBSuhail@users.noreply.github.com>
2024-09-16 21:09:08 +0530

    Time: 3 ms (74.51%), Space: 8.6 MB (84.81%) - LeetHub

--- 0032-longest-valid-parentheses/0032-longest-valid-parentheses.cpp
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.cpp
@@ -1,46 +1,19 @@
 class Solution {
 public:
     int longestValidParentheses(string s) {
-        int op=0,cl=0,len=0,n=s.size(),len2=0;
-        for(int i=0;i<n;i++)
-        {
-            if(s[i]=='(')
-            {
-                op++;
-            }else
-            {
-                cl++;
-            }
-            if(cl>op)
-            {
-                op=0,cl=0;
-            }
-            else if(op==cl)
-            {
-                len=max(len,cl*2);
-            }
+        int op = 0, cl = 0, len = 0, n = s.size(), len2 = 0;
+        for (int i = 0; i < n; i++) {
+            s[i] == '(' ? op++ : cl++;
+            if (cl > op) op = cl = 0;
+            else if (op == cl) len = max(len, cl * 2);
         }
-
-
-        for(int i=n;i>=0;i--)
-        {
-            if(s[i]==')')
-            {
-                cl++;
-            }
-            else
-            {
-                op++;
-            }
-            if(op>cl)
-            {
-                op=0,cl=0;
-            }
-            else if(op==cl)
-            {
-                len2=max(len2,2*op);
-            }
-
-        }return max(len,len2);
+       op = cl = 0;
+        for (int i = n - 1; i >= 0; i--) {
+            s[i] == ')' ? cl++ : op++;
+            if (op > cl) op = cl = 0;
+            else if (op == cl) len2 = max(len2, op * 2);
+        }
+        
+        return max(len, len2);
     }
 };
