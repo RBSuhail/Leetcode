23ace2c51f98587a24b52f50371cd48e34bf08d2
RB SUHAIL <114150506+RBSuhail@users.noreply.github.com>
2024-09-16 22:16:46 +0530

    Time: 109 ms (78.34%), Space: 63.1 MB (42.35%) - LeetHub

--- 1582-design-browser-history/1582-design-browser-history.cpp
+++ 1582-design-browser-history/1582-design-browser-history.cpp
@@ -42,11 +42,3 @@ public:
         }return curr->val;
     }
 };
-
-/**
- * Your BrowserHistory object will be instantiated and called as such:
- * BrowserHistory* obj = new BrowserHistory(homepage);
- * obj->visit(url);
- * string param_2 = obj->back(steps);
- * string param_3 = obj->forward(steps);
- */
