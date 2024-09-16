ea7aeded3f6d4c65e398282799b173fb87e05780
RB SUHAIL <114150506+RBSuhail@users.noreply.github.com>
2024-09-16 14:06:48 +0530

    Time: 136 ms (26.61%), Space: 57.8 MB (67.15%) - LeetHub

--- 0310-minimum-height-trees/0310-minimum-height-trees.cpp
+++ 0310-minimum-height-trees/0310-minimum-height-trees.cpp
@@ -1,62 +1,51 @@
 class Solution {
 public:
-
     vector<int> findMinHeightTrees(int n, vector<vector<int>>& edges) {
-        if(n==1) return vector<int>{0};
-        if(n==2) return edges[0];
-
-        vector<vector<int>> adjList(n);
-
-        vector<int> inDegree(n,0);
-        for(int i=0;i<edges.size();++i){
-            adjList[edges[i][0]].push_back(edges[i][1]);
-            adjList[edges[i][1]].push_back(edges[i][0]);
-            inDegree[edges[i][0]]++;
-            inDegree[edges[i][1]]++;
+        
+        if(n==0)return {};
+        if(n==1)return {0};
+        vector<int>adj[n];
+        vector<int>indegree(n,0);
+        for(auto it:edges)
+        {
+            int u=it[0];
+            int v=it[1];
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+            indegree[u]++;
+            indegree[v]++;
+        }
+        queue<int>bfs;
+        for(int i=0;i<n;i++)
+        {
+            if(indegree[i]==1)bfs.push(i);
         }
 
-        queue<int> bfs;
-        for(int i=0;i<inDegree.size();++i){
-            if(inDegree[i]==1){
-                bfs.push(i);
-                inDegree[i]--;
-            }
-        }
-
-        vector<int> v;
-        while(!bfs.empty()){
-            int s=bfs.size();
-            
-            while(s){
-                int front=bfs.front();
+        
+        while(n>2)
+        {
+            int sz=bfs.size();
+            n-=sz;
+            while(sz--)
+            {
+                int node=bfs.front();
                 bfs.pop();
-                for(int i=0;i<adjList[front].size();++i){
-                    inDegree[adjList[front][i]]--;
-                    if(inDegree[adjList[front][i]]==1){
-                        bfs.push(adjList[front][i]);
+                
+                for(auto it:adj[node])
+                {   indegree[it]--;
+                    if(indegree[it]==1)
+                    {
+                        bfs.push(it);
                     }
                 }
-                --s;
-            }
-            if(bfs.size()<=2 && bfs.size()>=1){
-                v.clear();
-                int size=bfs.size();
-                while(size)
-                {
-                    int e=bfs.front();
-                    v.push_back(e);
-                    bfs.pop();
-                    bfs.push(e);
-                    --size;
-                }
             }
         }
-
-        return v;
+        vector<int>result;
+        while(!bfs.empty())
+        {   int node=bfs.front();
+            result.push_back(node);
+            bfs.pop();
+        }
+        return result;
     }
 };
-
-
-
-
-
