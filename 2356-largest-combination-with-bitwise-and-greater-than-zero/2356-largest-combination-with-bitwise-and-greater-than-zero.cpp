c1f1012b755fe4fd8824e791e3ee4e9b41a6e714
RB SUHAIL <114150506+RBSuhail@users.noreply.github.com>
2024-11-07 10:50:19 +0530

    Time: 31 ms (60.45%), Space: 60.1 MB (88.86%) - LeetHub

--- 2356-largest-combination-with-bitwise-and-greater-than-zero/2356-largest-combination-with-bitwise-and-greater-than-zero.cpp
+++ 2356-largest-combination-with-bitwise-and-greater-than-zero/2356-largest-combination-with-bitwise-and-greater-than-zero.cpp
@@ -1,16 +1,19 @@
 class Solution {
 public:
     int largestCombination(vector<int>& candidates) {
-        vector<int>cnt(24,0);
+        // vector<int>cnt(24,0);
+        
+         int ans=0;
         for(int i=0;i<24;i++)
-        {
+        {   int cnt=0;
             for(auto it:candidates)
             {
                 if(it&1<<i)
-                cnt[i]++;
+                cnt++;
             }
+            ans=max(ans,cnt);
         }
-        int ans=*max_element(begin(cnt),end(cnt));
+       
         return ans;
     }
 };
